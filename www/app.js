a503df168d5c5d6e4b724de570f2f0d417ed86e2
SamBobBarnes <54638834+SamBobBarnes@users.noreply.github.com>
2021-02-19 16:33:03 -0600

    basic async working

--- www/app.js
+++ www/app.js
@@ -1,12 +1,32 @@
+//const searchQueryURL = 'https://api.github.com/search/users?q=sambobbarnes'; //https://api.github.com/search/repositories?q={query}{&page,per_page,sort,order}
+
+
+
+
+
+
+
+
+// Vue
 const app = Vue.createApp({
     data() {
         return {
-
+            searchQueryURL: 'https://api.github.com/search/users?q=sambobbarnes',
+            result: []
         }
     },
     methods: {
-
-
+        search: function() {
+            this.getResult()
+        },
+        getResult: async function () {
+            const response = await fetch(this.searchQueryURL)
+            const result = await response.json()
+            console.log(result)
+            result.items.forEach(i=>{
+                this.result.push(i)
+            })
+        }
     }
 })
 
